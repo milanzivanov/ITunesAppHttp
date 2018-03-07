2242bf3cce0fe76a10de49de314f35259205fb52
milan.zivanov <milan.zivanov@gmail.com>
2018-03-07 22:50:49 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my';
+  title = 'http trening with promises and with observarbles';
 }
