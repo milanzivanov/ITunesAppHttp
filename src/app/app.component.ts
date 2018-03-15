3c05d3bcfd1731dac5d5ab57dbc26d6d9f52dccd
milan.zivanov <milan.zivanov@gmail.com>
2018-03-15 23:15:58 +0100

    ngFor and ngFor in ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'http trening with promises(branch 1) and observarbles(branch 2)';
+  title = `*ngFor example & *ngFor in ngFor`;
+
+  // simple *ngFor example
+  persons: Array<any> = [
+    {
+      'name': 'Milan Zivanov me'
+    },
+    {
+      'name': 'Ivan Zivanov my big brother'
+    },
+    {
+      'name': 'Mile Z'
+    },
+    {
+      'name': 'Milica Z'
+    },
+    {
+      'name': 'Marija B'
+    }
+  ];
+
+  // *ngFor in ngFor example grouped
+  peopleByCountry: any[] = [
+    {
+      'country': 'UK',
+      'people': [
+        {
+          'name': 'Douglas  Pace'
+        },
+        {
+          'name': 'Mcleod  Mueller'
+        },
+      ]
+    },
+    {
+      'country': 'US',
+      'people': [
+        {
+          'name': 'Day  Meyers'
+        },
+        {
+          'name': 'Aguirre  Ellis'
+        },
+        {
+          'name': 'Cook  Tyson'
+        }
+      ]
+    }
+  ];
 }
