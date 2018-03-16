cfe78acb97f4362ed6e855a681a1334b31f345bb
milan.zivanov <milan.zivanov@gmail.com>
2018-03-16 12:44:28 +0100

    ngIf and ngSwitch examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'http trening with promises(branch 1) and observarbles(branch 2) now ther is lot more branches of agular staff';
+
+
+  people: Array<any> = [
+    {
+      'name': 'Douglas  Pace',
+      'age': 35,
+      'country': 'SER'
+    },
+    {
+      'name': 'Mcleod  Mueller',
+      'age': 32,
+      'country': 'GER'
+    },
+    {
+      'name': 'Day  Meyers',
+      'age': 21,
+      'country': 'UK'
+    },
+    {
+      'name': 'Aguirre  Ellis',
+      'age': 34,
+      'country': 'SER'
+    },
+    {
+      'name': 'Cook  Tyson',
+      'age': 32,
+      'country': 'USA'
+    }
+  ];
+
+
 }
+
