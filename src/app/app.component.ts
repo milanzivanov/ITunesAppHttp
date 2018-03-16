ed947d770b2f7bc897ec504e72aa6ecfa8cf3959
milan.zivanov <milan.zivanov@gmail.com>
2018-03-16 14:31:25 +0100

    ngStyle and ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'http trening with promises(branch 1) and observarbles(branch 2) now ther is lot more branches of agular staff';
-}
+
+  people: any[] = [
+    {
+      'name': 'Douglas  Pace',
+      'country': 'UK'
+    },
+    {
+      'name': 'Mcleod  Mueller',
+      'country': 'USA'
+    },
+    {
+      'name': 'Day  Meyers',
+      'country': 'HK'
+    },
+    {
+      'name': 'Aguirre  Ellis',
+      'country': 'UK'
+    },
+    {
+      'name': 'Cook  Tyson',
+      'country': 'USA'
+    }
+  ];
+
+  // Our getColor function returns different colors depending on the country passed in
+  getColor(country) {
+    switch (country) {
+      case 'UK':
+        return 'green';
+      case 'USA':
+        return 'blue';
+      case 'HK':
+        return 'red';
+    }
+  }
+
+ }
